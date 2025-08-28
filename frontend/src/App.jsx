e1f6cf1fcb885ba389795722c7d001b5f7db9460
roshan-wankhede000 <roshanwankhde212@gmail.com>
2025-08-28 20:38:05 +0530

    App.css imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "./app.css"
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from './component/Register'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
