d2c2762f7e4602884ef8d500bd36c2b9ee949e42
roshan-wankhede000 <roshanwankhde212@gmail.com>
2025-08-29 12:24:02 +0530

    call back url added

--- backend/controller/passportController.js
+++ backend/controller/passportController.js
@@ -6,7 +6,7 @@ const User = require('../model/registerModel');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://stylocart.onrender.com/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
